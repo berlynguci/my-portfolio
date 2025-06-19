efb7a25bf804530d0598494edf0e5e04b80e6d1d
BabyBokChoy8 <berlinsanity101@gmail.com>
2025-06-19 09:45:06 +0800

    initial commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,24 +1,27 @@
 import { useRef } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import pic1 from "../../assets/pic01.jpg";
+import pic2 from "../../assets/pic02.jpg";
+import pic3 from "../../assets/pic03.jpg";
 
 const items = [
   {
     id: 1,
     title: "Sales Commission Tracker",
-    img: "pic01.jpg",
+    img: pic1,
     desc: "A system that automates commission payouts for unit managers, making the process more accurate, secure, and easier to track.",
   },
   {
     id: 2,
     title: "Medical Billing System",
-    img: "pic03.jpg",
+    img: pic3,
     desc: "A system that records patient details, services, and medicines taken, while automatically creating their bill. It helps make billing faster, more accurate, and better for the patient. ",
   },
   {
     id: 3,
     title: "Online Pharmacy",
-    img: "pic02.jpg",
+    img: pic2,
     desc: "A system that makes it easier to order medical products from the pharmacy. It helps the pharmacy work faster, shortens customer wait times, and improves the overall experience.",
   },
 ];
